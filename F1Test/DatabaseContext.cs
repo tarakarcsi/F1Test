1a639be318e92098dc4e40b2b906fa2dc4fe28fa
Taraszovics Károly <tarakarcsi94@gmail.com>
2020-11-27 13:04:26 +0100

    sql connected

---
+++ F1Test/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace F1Test
+{
+    public class DatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=MyDatabase.db");
+        }
+    }
+}
