ee07f7c855096672e3e760ba4689a6c480475193
tarakarcsi <tarakarcsi94@gmail.com>
2020-12-14 15:37:45 +0100

    insert 3 rows to database

--- F1Test/DatabaseContext.cs
+++ F1Test/DatabaseContext.cs
@@ -26,6 +26,40 @@ namespace F1Test
                 e.Property(entity => entity.HasPayed);
             });
             base.OnModelCreating(modelBuilder);
+
+
+            var team1 = new Team()
+            {
+                Id = System.Guid.NewGuid(),
+                Titles = 2,
+                Name = "McLaren Mercedes",
+                FoundationYear = 1934,
+                HasPayed = true
+            };
+
+            var team2 = new Team()
+            {
+                Id = System.Guid.NewGuid(),
+                Titles = 12,
+                Name = "Ferrari",
+                FoundationYear = 1928,
+                HasPayed = true
+            };
+
+            var team3 = new Team()
+            {
+                Id = System.Guid.NewGuid(),
+                Titles = 2,
+                Name = "Mercedes-Benz",
+                FoundationYear = 1932,
+                HasPayed = true
+            };
+
+            using (var context = new DatabaseContext())
+            {
+                context.AddRange(team1, team2, team3);
+                context.SaveChanges();
+            }
         }
     }
 }
