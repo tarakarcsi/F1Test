33c29897193f2116a2ceb5d7c7d2f382a1058fa5
tarakarcsi <tarakarcsi94@gmail.com>
2020-12-14 15:21:03 +0100

    create db table from model

--- F1Test/DatabaseContext.cs
+++ F1Test/DatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using F1Test.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,20 @@ namespace F1Test
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Filename=MyDatabase.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Team>().ToTable("Team", "f1test");
+            modelBuilder.Entity<Team>(e =>
+            {
+                e.HasKey(entity => entity.Id);
+                e.Property(entity => entity.Name);
+                e.Property(entity => entity.Titles);
+                e.Property(entity => entity.FoundationYear);
+                e.Property(entity => entity.HasPayed);
+            });
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
